18b5dfd0de53e9c58d91b1f8321aec3872f7ac83
michael rouger <mika.lm049@gmail.com>
2025-03-10 16:39:31 +0100

    setup ProPulse - Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,4 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :users, only: %i[show edit update]
+  resources :tests, only: %i[index show] do
+    resources :questions, only: %i[show]
+  end
+
 end
