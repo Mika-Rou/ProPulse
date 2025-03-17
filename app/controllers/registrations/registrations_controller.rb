62fb07927a6b1bc21964319f8390f2a29b01c403
Milan Compain <compainmil@gmail.com>
2025-03-17 15:42:13 +0100

    register page

--- app/controllers/registrations/registrations_controller.rb
+++ app/controllers/registrations/registrations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Sessions::RegistrationsController < Devise::RegistrationsController
+class Registrations::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -10,9 +10,9 @@ class Sessions::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -51,9 +51,9 @@ class Sessions::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   questions_path(1)
-  # end
+  def after_sign_up_path_for(resource)
+    question_path(1)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :user_answers
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
